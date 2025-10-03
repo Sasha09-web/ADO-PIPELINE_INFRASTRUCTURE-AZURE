88fbb822d8d1638850b97f7706bf2551e7ed6cbf
Sasha09-web <beherasashabindu@gmail.com>
2025-10-04 01:38:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 
 
-resource "azurerm_resource_group" "rgpipeline2" {
-  name     = "pipeline_rg2"
+resource "azurerm_resource_group" "rgpipeline3" {
+  name     = "pipeline_rg3"
   location = "West Europe"
 }
 
 
-resource "azurerm_storage_account" "stgpipeline2" {
-    depends_on = [ azurerm_resource_group.rgpipeline2 ]
-  name                     = "pipelinestg2"
-  resource_group_name      = "pipeline_rg2"
+resource "azurerm_storage_account" "stgpipeline3" {
+    depends_on = [ azurerm_resource_group.rgpipeline3 ]
+  name                     = "pipelinestg3"
+  resource_group_name      = "pipeline_rg3"
   location                 = "West Europe"
   account_tier             = "Standard"
   account_replication_type = "GRS"
